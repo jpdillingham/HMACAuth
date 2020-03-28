7e15face9f01a4de44d965be86ccf12fc8211025
JP Dillingham <jpdillingham@gmail.com>
2020-03-28 12:15:58 -0500

    add tests, increment WIP

--- src/Startup.cs
+++ src/Startup.cs
@@ -36,7 +36,7 @@ namespace HMACAuth
             services.AddControllers();
 
             services.AddAuthentication("HMAC")
-                .AddScheme<ApiKeyAuthenticationOptions, ApiKeyAuthenticationHandler>("HMAC", options => { });
+                .AddScheme<HMACAuthenticationOptions, HMACAuthenticationHandler>("HMAC", options => { });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -71,41 +71,97 @@ namespace HMACAuth
         }
     }
 
-    public class ApiKeyAuthenticationHandler : AuthenticationHandler<ApiKeyAuthenticationOptions>
+    public class HMACAuthenticationHandler : AuthenticationHandler<HMACAuthenticationOptions>
     {
-        public ApiKeyAuthenticationHandler(IOptionsMonitor<ApiKeyAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder urlEncoder, ISystemClock systemClock)
+        public HMACAuthenticationHandler(IOptionsMonitor<HMACAuthenticationOptions> options, ILoggerFactory logger, UrlEncoder urlEncoder, ISystemClock systemClock)
             : base(options, logger, urlEncoder, systemClock)
         {
             Secrets.Add("088546f2-aba0-49d0-9323-4b07bf926ab1", "pWN4NAwKk+SUokEvDNZ4fcX3t2ozTFPgypXKchk1ulM=");
         }
 
         private Dictionary<string, string> Secrets { get; } = new Dictionary<string, string>();
+        private string[] RequiredHeaders { get; } = new[] {
+            HeaderNames.Authorization,
+            HeaderNames.Date,
+            HeaderNames.RequestId,
+            HeaderNames.ContentLength,
+            HeaderNames.ContentMD5
+        };
 
         protected override Task<AuthenticateResult> HandleAuthenticateAsync()
         {
-            var header = Request.Headers[HeaderNames.Authorization].SingleOrDefault();
+            static Task<AuthenticateResult> Fail(string reason, AuthenticationProperties props = null) =>
+                Task.FromResult(AuthenticateResult.Fail(reason, props));
 
-            if (!TryValidateAuthorizationHeader(header, out var credentials))
+            if (!TryValidateRequiredHeaders(Request, RequiredHeaders, out var missingHeaders))
             {
-                return Task.FromResult(AuthenticateResult.Fail("Invalid Authorization header value, expected 'HMAC <access key>:<message digest>'"));
+                return Fail($"Missing one or more required headers: {missingHeaders}");
+            }
+
+            if (!TryValidateRequestTime(Request, out var message))
+            {
+                return Fail($"something about request being too old");
+            }
+
+            if (!TryValidateAuthorizationHeader(Request, out var credentials, out message))
+            {
+                return Fail($"Invalid Authorization header value: {message}");
             }
 
             if (!Secrets.ContainsKey(credentials.Key))
             {
-                return Task.FromResult(AuthenticateResult.Fail("Unrecognized access key"));
+                return Fail("Unrecognized access key");
             }
+
+            var secret = Secrets[credentials.Key];
+
+            // todo: compute digest and compare
 
             throw new NotImplementedException();
         }
 
-        private bool TryValidateAuthorizationHeader(string header, out (string Key, string Digest) credentials)
+        public bool TryValidateRequestTime(HttpRequest request, out string message)
+        {
+            message = null;
+
+            // todo: validate Date header against clock/drift settings to ensure it is within the window
+
+            return true;
+        }
+
+        public bool TryValidateRequiredHeaders(HttpRequest request, string[] requiredHeaders, out string missingHeaders)
+        {
+            missingHeaders = null;
+            var missing = requiredHeaders.Where(header => request.Headers[header] == string.Empty);
+
+            if (missing.Any())
+            {
+                missingHeaders = string.Join(" ,", missing);
+                return false;
+            }
+
+            return true;
+        }
+
+        public bool TryValidateAuthorizationHeader(HttpRequest request, out (string Key, string Digest) credentials, out string error)
         {
             credentials = (null, null);
+            error = "Unknown error";
 
+            var headers = request.Headers[HeaderNames.Authorization].ToList();
+
+            if (headers.Count > 1)
+            {
+                error = "Multiple Authorization headers provided with request";
+                return false;
+            }
+
+            var header = headers[0];
             var pattern = "^hmac [a-zA-Z0-9-]{36}:[a-zA-Z0-9]{64}$";
 
             if (!Regex.IsMatch(header, pattern, RegexOptions.IgnoreCase))
             {
+                error = $"Invalid format; expected 'HMAC <access key:36>:<message digest:64>', received {header}";
                 return false;
             }
 
@@ -115,6 +171,7 @@ namespace HMACAuth
 
             if (!Guid.TryParseExact(key, "D", out _))
             {
+                error = $"Access key is not a valid GUID/UUID; expected format '00000000-0000-0000-0000-000000000000', received {key}";
                 return false;
             }
 
@@ -123,7 +180,7 @@ namespace HMACAuth
         }
     }
 
-    public class ApiKeyAuthenticationOptions : AuthenticationSchemeOptions
+    public class HMACAuthenticationOptions : AuthenticationSchemeOptions
     {
 
     }
---
+++ tests/HMACAuthenticationHandlerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace HMACAuth.Tests
+{
+    public class HMACAuthenticationHandlerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
